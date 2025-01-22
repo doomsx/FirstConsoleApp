06e75e0c11f50e107f5b9e4fb0e8f12f63ea823a
Doms <dlmagat08@proton.me>
2025-01-22 22:28:18 +0800

    Initial commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var age = 23;
+            age = 24;
+            Console.WriteLine(age);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
